355f414c22ffcd70fac1aecfaf8375090846ce80
leexsh <851001285@qq.com>
2019-11-04 18:20:30 +0800

    NULL

--- hard/NULL.cpp
+++ hard/NULL.cpp
@@ -1,9 +1,16 @@
-
-
-// 还没有做任何一道Hard难度的题目
 #include<iostream>
+#include<vector>
+
 using namespace std;
+// 还没有做任何一道Hard难度的题目
+int print(vector<int> &vec){
+    for(auto i : vec){
+        cout<<i<<" ";
+    }
+}
 int main(){
+    vector<int> vec{12,3,4,4};
+    print(vec);
     cout<<"asd"<<endl;
     system("pause");
     return 0;
