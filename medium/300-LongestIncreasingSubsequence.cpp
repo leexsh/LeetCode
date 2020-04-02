0f0212767195653af83848e8793a37b0dcb6bf25
leexsh <851001285@qq.com>
2020-04-02 18:25:09 +0800

    up

---
+++ medium/300-LongestIncreasingSubsequence.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+/*
+LeetCode-300 题目：最长上升子序列
+    给定一个无序的整数数组，找到其中最长上升子序列的长度。
+    示例:
+        输入: [10,9,2,5,3,7,101,18]
+        输出: 4
+        解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+    说明:
+        可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+        你算法的时间复杂度应该为 O(n2) 。
+        进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+*/
+
+/*
+思路：
+
+ */
+
+// 动态规划
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.empty()){
+            return 0;
+        }
+        vector<int> dp(nums.size(), 1);
+        dp[0] = 1;
+        int maxlen = 1;
+        for(int i = 1; i < nums.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i], dp[j+1]);
+                }
+            }
+            maxlen = max(maxlen, dp[i]);
+        }
+        return maxlen;
+    }
+};
+int main(){
+
+    system("pause");
+    return 0;
+}
--- medium/347-TopKFrequentElements.cpp
+++ medium/347-TopKFrequentElements.cpp
@@ -18,15 +18,65 @@ LeetCode-347 题目：前k个高频元素
 思路：
 
  */
+struct cmp{
+    bool operator()(pair<int, int> &a, pair<int, int> &b){
+        return a.second > b.second;
+    }
+};
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-
+        map<int, int> m;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> que;
+        for(auto i : nums){
+            m[i]++;
+        }
+        for(auto ite = m.begin(); ite != m.end(); ite++){
+            que.push(make_pair(ite->first, ite->second));
+            if(que.size() > k){
+                que.pop();
+            }
+//            if(que.size() == k){
+//                if(ite->second > que.top().second){
+//                    que.pop();
+//                    que.push(make_pair(ite->first, ite->second));
+//                } else{
+//                    que.push(make_pair(ite->first, ite->second));
+//                }
+//            }
+        }
+        vector<int> vec;
+        while(!que.empty()){
+            vec.push_back(que.top().first);
+            que.pop();
+        }
+        return vec;
+    }
+};
+class Solution1 {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> m;
+        for(auto i : nums){
+            m[i]++;
+        }
+        vector<pair<int, int>> vec(m.begin(), m.end());
+        sort(vec.begin(), vec.end(), [](pair<int,int> &a, pair<int,int> &b){return a.second > b.second;});
+        vector<int> res;
+        auto a = vec.begin();
+        while (k--)
+        {
+            res.push_back(a->first);
+            a++;
+        }
+        return res;
     }
 };
 
 int main(){
-
+    vector<int> nums{1,1,1,2,2,3};
+    Solution s;
+    vector<int>  v = s.topKFrequent(nums, 2);
     system("pause");
     return 0;
 }
