87f8bb58dd9e25f66e022a5e57236903010f5f9f
leexsh <851001285@qq.com>
2020-04-05 22:24:32 +0800

    up

--- main.cpp
+++ main.cpp
@@ -30,30 +30,20 @@ public:
 };
 
 
-
-
-string core(string s, int i , int j ){
-    while(s[i] == s[j] && i >= 0 && j < s.size()){
-        i--, j++;
-    }
-    return s.substr(i+1, j - i - 1);
-}
-string longestPalindrome(string s) {
-    if(s.size() <= 1)
-    {
-        return s;
-    }
-    string str;
-    for (int i = 0; i < s.size()  ; ++i) {
-        string s1 = core(s, i, i);
-        string s2 = core(s, i,i +1);
-        string ss = s1.size() > s2.size() ? s1 : s2;
-        str = str.size() > ss.size() ? str : ss;
+class LFUCache {
+public:
+    LFUCache(int capacity) {
 
     }
-    return str;
-//    return s.substr(start, maxlen);
-}
+
+    int get(int key) {
+
+    }
+
+    void put(int key, int value) {
+
+    }
+};
 
 
 int main() {
